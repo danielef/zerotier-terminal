d0769610bb738aaf24689124697f284edd315989
Daniel Estevez <daniel.ef@gmail.com>
2022-04-29 01:43:13 -0400

    Testing default colors

--- ztconsole.py
+++ ztconsole.py
@@ -33,11 +33,11 @@ def retrieve_members(network_id,
 def main():
     curses.initscr()
     curses.start_color()
-#    curses.use_default_colors()
+    curses.use_default_colors()
 
     stdscr = curses.newpad(40,60)
     mypad_pos = 0
-
+    
     curses.init_pair(1, curses.COLOR_WHITE,  curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_GREEN,  curses.COLOR_BLACK)
     curses.init_pair(3, curses.COLOR_YELLOW, curses.COLOR_BLACK)
